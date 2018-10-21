feceefbbea3a061815c876d91a7c5f9f8f7c5330
Jason Crowley <crowley.json@gmail.com>
2018-10-20 22:45:54 -0400

    more refined search

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 console.log("starting content script");
 let bodyText = document.body.textContent.toLowerCase(); 
-if (bodyText.search(/\bagree\b.*\bterms\b/) !== -1 || bodyText.search(/\bagree\b.*\bprivacy policy\b/) !== -1) {
+if (bodyText.search(/(\bi\b)|(\byou\b).*(\bagree\b)|(\bunderstand\b)|(\backnowledge\b).*(\bterms\b)|(\bprivacy policy\b)/) !== -1) {
 	// bodyText.includes("agree") && (bodyText.includes("terms") || bodyText.includes("privacy policy")
 	// document.getElementById("password") || document.getElementById("pass") || document.getElementById("psw") || document.getElementById("new-password")
 
